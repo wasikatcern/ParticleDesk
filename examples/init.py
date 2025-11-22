ac74907a007bbff71972c7243748af4cd435e65a
Wasikul Islam <wasikul.islam@cern.ch>
2025-11-22 13:32:59 -0800

    Create init.py

---
+++ examples/init.py
@@ -0,0 +1,19 @@
+"""
+Physics analysis examples from CERN Open Data.
+"""
+
+from .examples_data import (
+    EXAMPLES,
+    get_example,
+    get_all_examples,
+    get_example_ids,
+    get_example_titles
+)
+
+__all__ = [
+    'EXAMPLES',
+    'get_example',
+    'get_all_examples',
+    'get_example_ids',
+    'get_example_titles'
+]
