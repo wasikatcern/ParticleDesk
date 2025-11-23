fdc85806e32d44a6beda519278d494b6025d03a2
Wasikul Islam <wasikul.islam@cern.ch>
2025-11-23 12:25:06 -0800

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ Analyze CERN Open Data with AI-powered analysis assistance
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 from io import StringIO
